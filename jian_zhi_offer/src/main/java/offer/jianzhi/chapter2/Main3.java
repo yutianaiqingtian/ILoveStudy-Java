015df666d353f1b359c1b0db02f713c57915a375
jhzhang <15671678163@163.com>
2018-05-17 12:38:34 +0800

    [update] 1. update format

--- jian_zhi_offer/src/main/java/offer/jianzhi/chapter2/Main3.java
+++ jian_zhi_offer/src/main/java/offer/jianzhi/chapter2/Main3.java
@@ -1,25 +1,27 @@
 package offer.jianzhi.chapter2;
 
 /**
- * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，
+ * 每一列都按照从上到下递增的顺序排序。请完成一个函数，
+ * 输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
  *
  * @author jhZhang
  * @date 2018/5/14
  */
 public class Main3 {
-public boolean Find(int target, int[][] array) {
-    int rolCount = array.length;
-    int colCount = array[0].length;
-    int i, j;
-    for (i = rolCount - 1, j = 0; i >= 0 && j < colCount; ) {
-        if (array[i][j] == target) {
-            return true;
-        } else if (array[i][j] > target) {
-            i--;
-        } else if (array[i][j] < target) {
-            j++;
+    public boolean Find(int target, int[][] array) {
+        int rolCount = array.length;
+        int colCount = array[0].length;
+        int i, j;
+        for (i = rolCount - 1, j = 0; i >= 0 && j < colCount; ) {
+            if (array[i][j] == target) {
+                return true;
+            } else if (array[i][j] > target) {
+                i--;
+            } else if (array[i][j] < target) {
+                j++;
+            }
         }
+        return false;
     }
-    return false;
-}
 }
