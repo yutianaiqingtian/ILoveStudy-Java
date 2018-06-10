0e34407351b47a73ee71d430ed337099b696faae
jhzhang <15671678163@163.com>
2018-06-10 23:53:31 +0800

    [update] 1. 增加剑指Offer23题 「从上往下打印出二叉树的每个节点，同层节点从左至右打印」解题思路和测试

---
+++ jian_zhi_offer/src/main/java/offer/jianzhi/chapter4/TreeNode.java
@@ -0,0 +1,16 @@
+package offer.jianzhi.chapter4;
+/**
+ * 二叉树的结点
+ *
+ * @author jhZhang
+ * @date 2018/6/10
+ */
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
---
+++ jian_zhi_offer/src/test/java/offer/jianzhi/chapter4/Main23Test.java
@@ -0,0 +1,14 @@
+package offer.jianzhi.chapter4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Main23Test {
+
+    @Test
+    public void testPrintFromTopToBottom() {
+        TreeNode root = null;
+        new Main23().PrintFromTopToBottom(root);
+    }
+}
